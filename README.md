385fa68f88bc5941670c805a064a7609b6d6dbba
Terry Rosen <tcrosen@gmail.com>
2015-12-29 13:13:00 -0500

    Documentation updates

--- README.md
+++ README.md
@@ -2,11 +2,23 @@
 
 A simple, clean starter kit for React web applications.
 
-**Technologies**
+## Target Audience
 
-* ES6 (via Babel)
-* React
-* Webpack
+| Javascript | ES2015/ES6 | React | Webpack |
+|---|---|---|---|
+| Intermediate | Beginner | Beginner | Beginner |
+
+## Project Overview
+
+| Stability | Full Stack | Tests | Deployable
+|---|---|---|---|
+| 1 | No | Yes | Yes
+
+*Stability Legend*
+
+1. Suitable for learning & hobby projects.  Does not include elaborate production build pipeline.
+2. Suitable for small to mid-sized projects.  Includes reasonable production build tools.
+3. Suitable for all projects. Extensive tooling included for large-scale application development and deployment.
 
 ## Get Started
 
@@ -17,26 +29,27 @@ $ npm install
 $ npm start
 ```
 
-Visit [http://localhost:8080]() to see the site running.  While this is running,
-and changes made to the code will cause Webpack to recompile the application
+Visit [http://localhost:8080]() to view the site.  
+
+> Tip: While this is running, any changes made to the code will cause Webpack to recompile the application
 and reload the browser.
 
-## Build Commands
-
-Starts development server in watch mode.
+## Workflow Commands
 
 ```sh
 $ npm start
 ```
 
-Builds the application for distribution into the `/dist` folder.
+Starts development server in watch mode.
 
 ```sh
 $ npm run build
 ```
 
-Cleans the distribution folder.
+Builds the application for distribution.
 
 ```sh
 $ npm run clean
 ```
+
+Cleans the distribution folder.
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,68 @@
 var path = require('path');
 
 module.exports = {
+  // Entry points are essentially files that are processed by Webpack as the
+  // "entrance" to your application. Note that even though there are many different
+  // files used to build the full application, Webpack finds these other files on
+  // its own based on module imports & exports.
   entry: {
     app: './src/index.js'
   },
 
+  // Output settings
   output: {
+    // [name] will be replaced with the entry point name defined above
     filename: '[name].js',
+
+    // This serves as the virtual path used by Webpack's dev server to reference
+    // your built files.
     publicPath: '',
+
+    // This is the physical path where your built application will be located.
     path: path.join(__dirname, 'dist'),
   },
 
+  // Webpack dev server configuration
   devServer: {
+    // The base directory for Webpack's dev server to locate files to serve
     contentBase: path.join(__dirname, 'src')
   },
 
+  // Webpack is mostly based around modules. This section teaches Webpack how to deal with
+  // different types of files and translate them into modules.
   module: {
     loaders: [{
+      // File type(s)
       test: /\.js$/,
-      exclude: /node_modules/,
+      // Directories to include (where to look for files)
+      include: path.join(__dirname, 'src'),
+      // Which loader(s) to use
       loader: 'babel',
+      // Parameters used by the loader
       query: {
+        // Out of the box, Babel doesn't know how to interpret your files.
+        // These presets are actually collections of plugins, in this case we are
+        // telling Babel to parse React (JSX) and ES2015 syntax.
         presets: ['es2015', 'react']
       }
     }, {
       test: /\.scss$/,
-      exclude: /node_modules/,
+      include: path.join(__dirname, 'src'),
+      // Tip: Loaders are interpreted from right-to-left.
+      // This means the SASS loader will first transpile down to CSS,
+      // then the CSS loader will transpile down to inline styles.
+      // This technique results in no external CSS files served by your application.
       loaders: ['style', 'css', 'sass']
     }, {
       test: /\.json$/,
-      exclude: /node_modules/,
+      include: __dirname,
+      // Tip: include should be used where possible. In this case since
+      // our application reads our package.json file, it needs to look in the root
+      // directory, so we exclude the node_modules & dist folders.
+      exclude: [
+        /node_modules/,
+        /dist/
+      ],
       loader: 'json'
     }]
   },
